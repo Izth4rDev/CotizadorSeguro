200049fa6351b96a9be9931af2367b60b474173e
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2022-10-08 14:32:11 -0300

    validacion del formulario

--- js/app.js
+++ js/app.js
@@ -26,10 +26,38 @@ UI.prototype.llenarOpciones = () => {
         const opcion = document.createElement('option');
         opcion.value = i;
         opcion.innerText = `${i}`;
-        console.log(opcion);
         combo.appendChild(opcion);
     }
 
+}
+
+//muestra alertas en pantalla
+
+UI.prototype.mostrarMensaje = (mensaje, tipo) =>{
+
+    const div = document.createElement('div');
+
+    if(tipo === 'error'){
+
+        div.classList.add('error');
+
+    }else{
+
+        div.classList.add('correcto');
+    }
+        
+    div.classList.add('mensaje','mt-10');
+    div.textContent = mensaje;
+
+    //insertar en HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div,document.querySelector('#resultado'));
+
+    setTimeout(()=>{
+
+        div.remove();
+
+    },3000);
 
 
 }
@@ -41,3 +69,41 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones();
 
 });
+
+//event listener
+
+eventListener();
+
+function eventListener (){
+
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+
+}
+
+function cotizarSeguro (e){
+
+    e.preventDefault();
+
+    //leer la marca seleccionada 
+    const marca = document.querySelector('#marca').value;
+    //leer año seleccionado
+    const year= document.querySelector('#year').value;
+    console.log(year);
+    //leer tipo de covertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value; //'input[name="tipo"]:checked' nos permite seleccionar el valor de un radio cuyo name sea = tipo
+    console.log(tipo);
+
+    if(marca === '' || year === '' || tipo === ''){
+
+        console.log('no paso la validacion');
+        ui.mostrarMensaje('todos los campos son obligatorios', 'error');
+        return;
+
+    }
+
+        ui.mostrarMensaje('cotizando.....', 'correcto');
+    
+}
+
+
